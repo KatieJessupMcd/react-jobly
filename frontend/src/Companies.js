e580a2a1d39343033cd475ed1443a56b699984af
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-18 13:41:00 -0700

    added start of pagination for companies

--- frontend/src/Companies.js
+++ frontend/src/Companies.js
@@ -4,28 +4,28 @@ import SearchForm from './SearchForm';
 import JoblyApi from './JoblyApi';
 // import companyListBackground from './companyListBackground.jpg';
 
-
 // var styles = {
 //   body: {
-//     backgroundColor: 'gray', 
+//     backgroundColor: 'gray',
 //     backgroundSize: 'fill',
 //     backgroundRepeat: 'no repeat',
 //     height: '100vh',
-//     width: '100%', 
-    
+//     width: '100%',
+
 //   }}
 class Companies extends Component {
   constructor(props) {
     super(props);
-    this.state = { companies: [] };
+    this.state = { companies: [], currentPage: 1, companiesPerPage: 10 };
 
     this.searchCompanies = this.searchCompanies.bind(this);
+    this.handleClick = this.handleClick.bind(this); 
   }
 
   async componentDidMount() {
-  //   for(let i in styles.body){
-  //     document.body.style[i] = styles.body[i];
-  // }
+    //   for(let i in styles.body){
+    //     document.body.style[i] = styles.body[i];
+    // }
     try {
       this.setState({
         companies: await JoblyApi.getAllCompanies()
@@ -39,7 +39,7 @@ class Companies extends Component {
     }
   }
   // async componentWillUnmount(){
-  //   document.body.style.background = null;   
+  //   document.body.style.background = null;
   // }
 
   async searchCompanies(query) {
@@ -54,6 +54,12 @@ class Companies extends Component {
     }
   }
 
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    }); 
+  }
+
   render() {
     // let backgroundStyle = {
     //   background: `url(${companyListBackground})`,
@@ -62,13 +68,28 @@ class Companies extends Component {
     //   height: '100vh',
     //   width: '100%'
     // };
+    let { companies, currentPage, companiesPerPage } = this.state;
+
+    // Logic for displaying companies
+    let indexOfLastCompany = currentPage * companiesPerPage;
+    let indexOfFirstCompany = indexOfLastCompany - companiesPerPage;
+    let currentCompanies = companies.slice(
+      indexOfFirstCompany,
+      indexOfLastCompany
+    );
+
+    // Logic to display page numbers
+    let pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(companies.length / companiesPerPage); i++) {
+      pageNumbers.push(1);
+    }
     return (
       <div className="row">
         <div className="Companies col-md-8 offset-md-2">
           <div className="Companies-Info">
             <SearchForm handleSearch={this.searchCompanies} />
-            {this.state.companies.length > 0 ? (
-              this.state.companies.map(c => (
+            {currentCompanies.length > 0 ? (
+              currentCompanies.map(c => (
                 <CompanyCard
                   key={c.handle}
                   handle={c.handle}
@@ -82,6 +103,15 @@ class Companies extends Component {
             )}
           </div>
         </div>
+        <ul id="page-numbers">
+          {pageNumbers.map(number => {
+            return (
+              <li key={number} id={number} onClick={this.handleClick}>
+                {number}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
